d90bb5e246de6e6e35010de13cacd606810698d9
alanaman <40354271+alanaman@users.noreply.github.com>
2024-09-15 11:43:12 +0530

    bugfix: game over on castle destroyed

--- Assets/scripts/Actors/Castle.cs
+++ Assets/scripts/Actors/Castle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Castle : MonoBehaviour
 {
-    private void OnDestroy()
+    public void OnCastleDestroyed()
     {
         GameManager.I.OnCastleDestroyed();
     }
